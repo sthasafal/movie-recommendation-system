6af6ac52e15d74adf1ddc3da53b0f984271a3cdd
Safal Shrestha <shresthasafal72@gmail.com>
2025-12-19 00:01:31 -0800

    Update recommend_runner.py

--- Backend/src/recommenders/recommend_runner.py
+++ Backend/src/recommenders/recommend_runner.py
@@ -2,8 +2,9 @@ from recommenders.utils.matrix_builder import load_data, create_user_movie_matri
 from recommenders.memory_based.memory_cf import MemoryCF
 from recommenders.model_based.svd_cf import SVD_CF
 
+
 def run_collaborative():
-    movies, ratings = load_data()
+    _, ratings = load_data()
     matrix = create_user_movie_matrix(ratings)
 
     print("\n=== Memory-Based CF ===")
@@ -11,11 +12,11 @@ def run_collaborative():
     print(mem.recommend_user_user(1, top_n=5))
 
     print("\n=== SVD CF ===")
-    svd = SVD_CF(n_components=20)
-    svd.fit(matrix)
-    print(svd.recommend(1, matrix, top_n=5))
+    svd = SVD_CF()
+    print(svd.recommend(1, top_n=5))
 
-    print("\n Collaborative Filtering Completed!")
+    print("\nCollaborative Filtering Completed!")
+
 
 if __name__ == "__main__":
     run_collaborative()
